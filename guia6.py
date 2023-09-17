e72275cd20e2474f77cfc832929136859aa77a6b
NickDaemon <proznicolas1993@gmail.com>
2023-09-17 18:17:53 -0300

    complete factorial

--- guia6.py
+++ guia6.py
@@ -6,8 +6,6 @@ def raizDe2():
     raizDe2: float = round(math.sqrt(2),4)
     return raizDe2
 
-print (raizDe2())
-
 #EJERCICIO 1.2)
 def imprimir_hola():
     return "hola"
@@ -22,6 +20,30 @@ def imprimir_verso():
 print (imprimir_verso())
 
 #EJERCICIO 1.4)
+# Ejercicio 1.4)
+def factorial(x: int) -> int:
+    res: int = 1
+    for i in range(1, x+1, 1):
+        res *= i
+    return res
+
+def factorial_de_dos() -> int:
+    return factorial(2)
+
+#EJERCICIO 1.5)
+def factorial_3() -> int:
+    return factorial(3)
+
+#EJERCICIO 1.6)
+def factorial_4() -> int:
+    return factorial(4)
+
+#EJERCICIO 1.7)
+def factorial_5() -> int:
+    return factorial(5)
+
+
+
 
 
  
