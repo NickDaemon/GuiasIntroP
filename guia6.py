1784131c96d0bba87f5d118a5009d90c78deda7c
NickDaemon <proznicolas1993@gmail.com>
2023-09-17 16:26:38 -0300

    comienzo python

---
+++ guia6.py
@@ -0,0 +1,38 @@
+import math
+#                                   GUIA 6 INTRO A PYTHON
+
+# Ejercicio 1.1)
+def raizDe2():
+    raizDe2: float = round(math.sqrt(2),4)
+    return raizDe2
+
+print (raizDe2())
+
+#EJERCICIO 1.2)
+def imprimir_hola():
+    return "hola"
+
+print (imprimir_hola())
+
+#EJERCICIO 1.3)   
+def imprimir_verso():
+    verso: str = " Brought out the pink Lamborghini just to race with Chyna\n Brought the Wraith to Chyna\n just to race in China\n Lil' bad Trini bitch but she mixed with China\n Real thick vagina, smuggle bricks to China"
+    return verso
+    
+print (imprimir_verso())
+
+#EJERCICIO 1.4)
+
+
+ 
+
+
+    
+
+
+
+  
+
+
+
+
